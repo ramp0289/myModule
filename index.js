21ffb4e73a8da1c2a6bcc5cae452085c79d95b1f
Happlabs <repoproyectos@happ.cl>
2023-11-06 14:54:34 -0300

    changes

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ class Color {
     }
 }
 const allColors = [
-    new Color('brightred', '#E74C3C'),
-    new Color('soothingpurple', '#9B59B6'),
-    new Color('skyblue', '#5DADE2'),
-    new Color('leafygreen', '#48C9B0'),
-    new Color('sunkissedyellow', '#F4D03F'),
     new Color('rene', '#D7DBDD'),
 ];  
 
